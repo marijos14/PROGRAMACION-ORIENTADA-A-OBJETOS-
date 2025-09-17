34ff0b70dc92d9e4a8ace7e5f7571e1b0706a014
acosta-jc <caylos162@gmail.com>
2025-09-17 15:21:29 -0600

    Pruebas p4

--- PRACTICA-04/PRACTICA/Ventana.java
+++ PRACTICA-04/PRACTICA/Ventana.java
@@ -3,17 +3,8 @@ import java.awt.event.*;
 
 public class Ventana extends JFrame {
     JButton boton;
-    Punto puntoA;
-    Punto puntoB;
-    double distancia;
-    Mensajes men;
     
     public Ventana(Punto puntoA, Punto puntoB, double distancia, Mensajes men) {
-
-        this.puntoA = puntoA;
-        this.puntoB = puntoB;
-        this.distancia = distancia;
-        this.men = men;
 
         setTitle("Distancia entre dos puntos");
         setSize(300, 200);
