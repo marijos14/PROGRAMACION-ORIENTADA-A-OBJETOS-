65e7f18c2442cbf247f5b8f3751ade4093a26b58
acosta-jc <caylos162@gmail.com>
2025-11-10 19:24:08 -0600

    Package de la práctica 09 y 10

--- PROYECTO-02/mx/unam/fi/poo/proyecto02/MainApp.java
+++ PROYECTO-02/mx/unam/fi/poo/proyecto02/MainApp.java
@@ -5,8 +5,19 @@ public class MainApp{
 
         Empleado empleado = new EmpleadoAsalariado("Karla", "Panini", "223456", 500.8);
         System.out.println(empleado);
+        System.out.println();
 
         empleado = new EmpleadoPorComision("Juan Gabriel", "Aguilera", "909090", 1000000, 0.7);
         System.out.println(empleado);
+        System.out.println();
+
+        //Esto se añadió después para el reporte
+        empleado = new EmpleadoAsalariado("Jhonny", "Deep", "1515", -14);
+        System.out.println(empleado);
+        System.out.println();
+
+        empleado = new EmpleadoPorComision("Slim", "Shady", "4444", 500000, 1.5);
+        System.out.println(empleado);
+        System.out.println();
     }
 }
